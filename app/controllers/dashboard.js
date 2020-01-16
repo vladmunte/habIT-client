927f4ec12f17728cb4fe6049a0f8a7880714f520
Vlad Munte <vlad.munte@luminos.software>
2020-01-16 17:06:54 +0200

    fix requests and add new chart on dashboard

--- app/controllers/dashboard.js
+++ app/controllers/dashboard.js
@@ -1,9 +1,75 @@
 import Controller, {inject as controller} from '@ember/controller';
 import { computed } from '@ember/object';
-import Ember from 'ember';
+// import fetch from 'fetch';
 
 export default Controller.extend({
   // ajax: Ember.inject.service(),
+  sportTotalDays: null,
+  lifestyleTotalDays: null,
+  financialTotalDays: null,
+  socialTotalDays: null,
+  cultureTotalDays: null,
+  init() {
+    this._super(...arguments);
+    this.get('sportDaysFetch');
+    this.get('lifestyleDaysFetch');
+    this.get('financialDaysFetch');
+    this.get('socialDaysFetch');
+    this.get('cultureDaysFetch');
+  },
+
+  sportDaysFetch: computed(async function() {
+    return fetch('habits/sport_habits_days')
+      .then((response) => {
+        return response.json();
+      }).then(dayz => {
+        let noDays = dayz.days;
+        // document.getElementsByClassName('test123456')[0].innerHTML = noDays;
+        this.set('sportTotalDays', noDays);
+        return noDays;
+      });
+  }),
+  lifestyleDaysFetch: computed(async function() {
+    return fetch('habits/lifestyle_habits_days')
+      .then((response) => {
+        return response.json();
+      }).then(dayz => {
+        let noDays = dayz.days;
+        this.set('lifestyleTotalDays', noDays);
+        return noDays;
+      });
+  }),
+  financialDaysFetch: computed(async function() {
+    return fetch('habits/financial_habits_days')
+      .then((response) => {
+        return response.json();
+      }).then(dayz => {
+        const noDays = dayz.days;
+        this.set('financialTotalDays', noDays);
+        return noDays;
+      });
+  }),
+  socialDaysFetch: computed(async function() {
+    return fetch('habits/social_habits_days')
+      .then((response) => {
+        return response.json();
+      }).then(dayz => {
+        const noDays = dayz.days;
+        this.set('socialTotalDays', noDays);
+        return noDays;
+      });
+  }),
+  cultureDaysFetch: computed(async function() {
+    return fetch('habits/culture_habits_days')
+      .then((response) => {
+        return response.json();
+      }).then(dayz => {
+        const noDays = dayz.days;
+        this.set('cultureTotalDays', noDays);
+        return noDays;
+      });
+  }),
+
   habitTracker: controller('habit-tracker'),
   lifestyleDays: computed(function() {  
     return this.habitTracker.get('lifestyleDays');
@@ -57,12 +123,36 @@ export default Controller.extend({
       ],
     };
   }),
+  priorityOptions2: computed('lifestyleTotalDays', 'sportTotalDays', 'financialTotalDays', 'socialTotalDays', 'cultureTotalDays', function() {
+    return {
+      labels: ['Lifestyle Days Checked', 'Sport Days Checked', 'Financial Days Checked', 'Social Days Checked', 'Culture Days Checked'],
+      datasets: [
+        {
+          data: [this.get('lifestyleTotalDays'), this.get('sportTotalDays'), this.get('financialTotalDays'), this.get('socialTotalDays'), this.get('cultureTotalDays')],
+          backgroundColor: [
+            'rgba(255, 99, 132)',
+            'rgba(54, 162, 235)',
+            'rgba(255, 206, 86)',
+            'rgba(75, 192, 192)',
+            'rgba(153, 102, 255)',
+            'rgba(255, 159, 64)'
+          ],
+          borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+          ],
+          borderWidth: 1,
+
+        }
+      ],
+    };
+  }),
   actions: {
-    sendRequest() {
-      return this.get('ajax').request('/habits/sport_habits_days', {
-        method: 'GET'
-      })
-    }
+    
   }
 
 });
